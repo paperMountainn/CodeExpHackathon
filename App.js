dcef6aa7bf1dc811697fc06ee1e416c5166960e4
whelxks <70426064+whelxks@users.noreply.github.com>
2021-06-16 02:39:49 +0800

    No functions yet

--- App.js
+++ App.js
@@ -4,12 +4,14 @@ import HomeScreen from "./src/screens/HomeScreen";
 import Screen1 from './src/screens/Screen1';
 import Screen2 from './src/screens/Screen2'
 import Screen3 from './src/screens/Screen3'
+import Screen5 from "./src/screens/Screen5";
 
 const navigator = createStackNavigator({
   Home: HomeScreen,
   Screen1: Screen1,
   Screen2: Screen2,
   Screen3: Screen3,
+  Screen5: Screen5,
 }, {
    initialRouteName: 'Home',
    defaultNavigationOptions: {
